831e66273711f1c4d205275c9c2795a5440de54e
hhh <1340545944@qq.com>
2019-07-08 19:21:20 +0800

    shorten version prefix

--- src/attribute.ts
+++ src/attribute.ts
@@ -12,7 +12,7 @@ export const updateVersion = (element: Element, attributeName: string) => {
     return newVersion;
 };
 
-const _VERSION_PREFIX = 'data-x-version-';
+const _VERSION_PREFIX = 'data-x-ver-';
 
 export const attributeSetters = new Map<string | symbol, AttributeSetter>([
     ['style', (element, style: string | StyleProperties) => {
