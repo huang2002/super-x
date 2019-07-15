37951b3faab3344ba209fed5fada45e9b8229730
hhh <1340545944@qq.com>
2019-07-15 19:41:21 +0800

    improve variable naming

--- src/router.ts
+++ src/router.ts
@@ -8,17 +8,17 @@ type RouteNames<T extends RouteMap> = T extends RouteMap<infer U> ? U : never;
 export const createRouter = <T extends RouteMap = RouteMap>(
     defaultRoute: RouteNames<T> | Value<RouteNames<T>>, routeMap: T
 ) => {
-    const routes = new Map<RouteNames<T>, any>(),
+    const routeCache = new Map<RouteNames<T>, any>(),
         router = ((defaultRoute as any)._isXV ?
             defaultRoute :
             Value.of(defaultRoute)
         ) as Value<RouteNames<T>>;
     return router.mapSync(routeName => {
-        if (routes.has(routeName)) {
-            return routes.get(routeName);
+        if (routeCache.has(routeName)) {
+            return routeCache.get(routeName);
         } else {
             const route = _normalize(_toArray(routeMap[routeName](router as unknown as Value<string>)));
-            routes.set(routeName, route);
+            routeCache.set(routeName, route);
             return route;
         }
     });
