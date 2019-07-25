6e64d6479402da590377443c3ed77bcd235b3414
hhh <1340545944@qq.com>
2019-07-25 19:20:20 +0800

    add default route callback

--- src/router.ts
+++ src/router.ts
@@ -1,25 +1,30 @@
 import { _toArray, _normalize } from "./utils";
 import { HistoryLike, createHistory } from "./histories";
 
-export type RouteCallback<T extends string = string> = (router: HistoryLike<T>) => any;
+export type RouteCallback<T extends string = string> = (history: HistoryLike<T>) => any;
 export type RouteMap<T extends string = string> =
     { [K in T]: RouteCallback<T>; } &
     { [path: string]: RouteCallback<T>; };
 
 export const createRouter = <T extends string = string>(
-    init: T | HistoryLike<T>, routeMap: RouteMap<T>
+    init: T | HistoryLike<T>, routeMap: RouteMap<T>, defaultCallback?: RouteCallback<T>
 ) => {
     const routeCache = new Map<T, any>(),
         history = ((init as any)._isXV ? init : createHistory(init as T)) as HistoryLike<T>;
+    let defaultRouteCache: any;
     return history.mapSync(routeName => {
         if (routeCache.has(routeName)) {
             return routeCache.get(routeName);
-        } else {
+        } else if (routeName in routeMap) {
             const route = _normalize(_toArray(
                 routeMap[routeName](history)
             ));
             routeCache.set(routeName, route);
             return route;
+        } else {
+            return defaultCallback && (
+                defaultRouteCache || (defaultRouteCache = defaultCallback(history))
+            );
         }
     });
 };
